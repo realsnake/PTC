46add5ce8b0dbd8c7115de231bbd616512db4b40
snake <bpchaoxin@163.com>
2012-01-10 14:17:23 +0800

    Initial Commit

---
+++ PTC/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PTC
+//
+//  Created by snake on 12-1-10.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PTC/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  PTC
+//
+//  Created by snake on 12-1-10.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
